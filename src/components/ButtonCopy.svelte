dcbc78cf10dcff96b367e685dab6163122391073
Candido Sales Gomes <candidosg@gmail.com>
2024-09-01 17:31:32 -0400

    separate component

---
+++ src/components/ButtonCopy.svelte
@@ -0,0 +1,67 @@
+<script lang="ts">
+	import anime from 'animejs';
+	import Checkmark from './icons/Checkmark.svelte';
+	import Copy from './icons/Copy.svelte';
+
+	const click = () => {
+		// Framer Motion
+		// transition: { type: "spring", duration: 0.5, bounce: 0.2 },
+
+		// spring - animejs
+		// mass, stiffness (tension), damping (friction), velocity
+		// spring is pretty slow
+		const timeline = anime.timeline({
+			easing: 'easeOutExpo',
+			autoplay: false,
+			duration: 300
+		});
+
+		timeline
+			.add({
+				targets: '.copy',
+				opacity: [1, 0],
+				scale: [1, 0.5]
+			})
+			.add({
+				targets: '.checkmark',
+				opacity: [0, 1],
+				scale: [0.5, 1]
+			})
+			.add({
+				targets: '.checkmark',
+				opacity: [1, 0],
+				scale: [1, 0.5],
+				delay: 300
+			})
+			.add({
+				targets: '.copy',
+				opacity: [0, 1],
+				scale: [0.5, 1]
+			});
+
+		timeline.play();
+	};
+</script>
+
+<button on:click={click}>
+	<Copy class="copy size-6" />
+	<Checkmark class="checkmark size-6" />
+</button>
+
+<style lang="postcss">
+	button {
+		background: white;
+		padding: 8px 16px;
+		border-radius: 8px;
+		font-size: 14px;
+		@apply relative flex h-12 w-12 cursor-pointer items-center justify-center;
+	}
+
+	:global(.copy, .checkmark) {
+		@apply absolute size-6;
+	}
+
+	:global(.checkmark) {
+		opacity: 0;
+	}
+</style>
