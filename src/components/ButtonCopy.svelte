84b125b478f1734af656d795fb8a88dee5f57972
Candido Sales Gomes <candidosg@gmail.com>
2024-09-01 17:46:00 -0400

    prevent multiple clicks

--- src/components/ButtonCopy.svelte
+++ src/components/ButtonCopy.svelte
@@ -3,7 +3,10 @@
 	import Checkmark from './icons/Checkmark.svelte';
 	import Copy from './icons/Copy.svelte';
 
+	let button: HTMLButtonElement;
+
 	const click = () => {
+		button.disabled = true;
 		// Framer Motion
 		// transition: { type: "spring", duration: 0.5, bounce: 0.2 },
 
@@ -16,45 +19,54 @@
 			duration: 300
 		});
 
+		// Reuse animations - Inspiratin Framer Motion
+		const variants = {
+			hidden: { opacity: [1, 0], scale: [1, 0.5] },
+			visible: { opacity: [0, 1], scale: [0.5, 1] }
+		};
+
 		timeline
 			.add({
 				targets: '.copy',
-				opacity: [1, 0],
-				scale: [1, 0.5]
+				opacity: variants.hidden.opacity,
+				scale: variants.hidden.scale
 			})
 			.add({
 				targets: '.checkmark',
-				opacity: [0, 1],
-				scale: [0.5, 1]
+				opacity: variants.visible.opacity,
+				scale: variants.visible.scale
 			})
 			.add({
 				targets: '.checkmark',
-				opacity: [1, 0],
-				scale: [1, 0.5],
+				opacity: variants.hidden.opacity,
+				scale: variants.hidden.scale,
 				delay: 300
 			})
 			.add({
 				targets: '.copy',
-				opacity: [0, 1],
-				scale: [0.5, 1]
+				opacity: variants.visible.opacity,
+				scale: variants.visible.scale
 			});
 
 		timeline.play();
+
+		// Avoid multiple clicks
+		timeline.finished.then(() => {
+			button.disabled = false;
+		});
 	};
 </script>
 
-<button on:click={click}>
+<button bind:this={button} on:click={click}>
 	<Copy class="copy size-6" />
 	<Checkmark class="checkmark size-6" />
 </button>
 
 <style lang="postcss">
 	button {
-		background: white;
-		padding: 8px 16px;
 		border-radius: 8px;
 		font-size: 14px;
-		@apply relative flex h-12 w-12 cursor-pointer items-center justify-center;
+		@apply relative flex h-12 w-12 cursor-pointer items-center justify-center bg-white px-4 py-2;
 	}
 
 	:global(.copy, .checkmark) {
